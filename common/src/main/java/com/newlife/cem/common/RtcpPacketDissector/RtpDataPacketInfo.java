e2d7bd21c4fc788d9cd336b323eff81559d27eb0
hungcm <chuhung1195@gmail.com>
2022-01-24 15:37:27 +0700

    add template code

--- common/src/main/java/com/newlife/cem/common/RtcpPacketDissector/RtpDataPacketInfo.java
+++
@@ -1,9 +0,0 @@
-package com.newlife.cem.common.RtcpPacketDissector;
-
-/**
- * RtpDataPacketInfo
- */
-public class RtpDataPacketInfo extends RtpPacketInfo{
-    public int timestamp;
-    public int sender_ssrc;
-}
